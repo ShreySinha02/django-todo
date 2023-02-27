1f3323d1110876f55636cb87ca06f69230ba36df
Shrey <53387208+ShreySinha02@users.noreply.github.com>
2023-02-28 02:45:16 +0530

    Update settings.py
    added '*' in allowed host

--- main/main/settings.py
+++ main/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_%%=4f67&b1n-1*qjc=)#5$@fsrgng@7e7l6u!ae427=+2vtz1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
